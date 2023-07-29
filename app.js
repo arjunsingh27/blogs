6d14d4b640029554e4b50d6a57e5287c0163bb4c
ArjunSingh <70803514+arjunsingh27@users.noreply.github.com>
2023-07-29 16:46:06 +0530

    Port Setup

--- app.js
+++ app.js
@@ -107,6 +107,6 @@ app.get("/posts/:postId", function (req, res) {
 
 
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
-});
+app.listen(process.env.PORT || 3000,function(req,res){
+  console.log("Server Started .........");
+})
